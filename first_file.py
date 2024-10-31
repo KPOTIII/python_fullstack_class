a0eb3e93a0900b42625512f7ef554aa7f5325341
Nazar <nagrebator2001@gmail.com>
2024-10-31 12:54:45 +0700

    developer 1 correct yourself

--- first_file.py
+++ first_file.py
@@ -1,4 +1,5 @@
 # Мой первый код на Python
-print('Hello cats1!')
-print('Hello cats2!')
-print('Hello cats3!')
+print('Hello cat 1!')
+print('Hello cat 2!')
+print('Hello cat 3!')
+print('Hello cat 4!')
